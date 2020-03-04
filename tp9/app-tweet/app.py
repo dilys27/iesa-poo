d4800e7bdc2622eaa5c4ffd8366ca518feaa8204
dilys27 <mac-dilys@MacBook-Pro-de-MAC-DILYS.local>
2020-03-04 16:47:10 +0100

    tp9 update

--- tp9/app-tweet/app.py
+++ tp9/app-tweet/app.py
@@ -15,23 +15,25 @@ clf=load('algo.joblib')
 def home():
     if request.method == "POST":
         tweet = request.form['tweet']
-        return redirect(request.url)
+        return redirect(request.url+'results')
     return render_template('home.html')
 
 # 0 - hate speech 1 - offensive language 2 - neither
 def return_probas():
-  probas = clf.predict_proba(tweet.lower()) 
+  probas = clf.predict_proba([tweet.lower()]) 
+  print(tweet)
+  print(probas)
   probas_dict = {
-                'hate speech':probas[0],
-                'offensive language':probas[1],
-                'neither':probas[2]
+                'hate speech':probas[0][0],
+                'offensive language':probas[0][1],
+                'neither':probas[0][2]
                 }
   return jsonify(probas_dict)
 
 
 @app.route('/results')
 def results():
-    return render_template('./results.html', tweet=tweet, probas_dict=return_probas())
+    return render_template('results.html', tweet=tweet, probas=return_probas())
 		
 if __name__ == '__main__':
     app.run(debug=True)
